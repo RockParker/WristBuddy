7d6f3740937d513947e0812d3810291fe8f52de7
AnthonyD2023 <dica0034@algonquinlive.com>
2026-03-18 21:26:54 -0400

    Implemented Player class and did example instamce. Also added player image.

--- entity.py
+++ entity.py
@@ -1,17 +1,21 @@
+import pygame as pg
 from abc import ABC, abstractmethod
 from scene import Scene
 
 class Entity(ABC):
-    @abstractmethod
-    def render(self):
-        pass
 
-    
+    def set_image(self, path: str): self.image = pg.image.load(rf'{path}')
+    def get_image(self) -> pg.Surface: return self.image
+    def get_pos(self) -> tuple: return (10, 10)
+
     def subscribe_to_scene(self, scene: Scene):
         scene.subcribe_entity(self)
 
+    def render(self, ctx: pg.Surface):
+        ctx.blit(self.image, self.get_pos())
 
 class EntityFactory(ABC):
+
     @abstractmethod
     def create(self) -> Entity:
         pass
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
+from player import Player, PlayerFactory
 
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 pygame.display.set_caption("Hello, Pygame!")
 
-
-
 running = True
+
+p = PlayerFactory().create()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255, 255, 255))
+    p.render(screen)
     pygame.display.flip()
 
 pygame.quit()
